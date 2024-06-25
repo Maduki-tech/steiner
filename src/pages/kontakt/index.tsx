c01bfd7282764a875dee41ea55e5a5a71e038725
Maduki-tech <d.schlueter1011@gmail.com>
2024-06-25 13:45:17 +0200

    fix form

--- src/pages/kontakt/index.tsx
+++ src/pages/kontakt/index.tsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "~/Components/Footer";
 import { AiOutlineWhatsApp } from "react-icons/ai";
 import Link from "next/link";
 import Header from "~/Components/Header";
+import { useRouter } from "next/router";
 
 export default function Index() {
-  const [vorName, setVorname] = React.useState("");
-  const [nachName, setNachname] = React.useState("");
-  const [nachricht, setNachricht] = React.useState("");
-  const form = React.useRef<HTMLFormElement>(null);
+  const vorNameRef = React.useRef<HTMLInputElement>(null);
+  const nachNameRef = React.useRef<HTMLInputElement>(null);
+  const nachrichtRef = React.useRef<HTMLTextAreaElement>(null);
 
   const handleMessageForWhatsapp = () => {
+
+    console.log(vorNameRef.current?.textContent);
+    // get text from input fields
+    const vorname = vorNameRef.current?.value || "";
+    const nachname = nachNameRef.current?.value || "";
+    const nachricht = nachrichtRef.current?.value || "";
+
     const message =
-      `Hallo, ich bin ${vorName} ${nachName} und ich möchte mit Ihnen zusammenarbeiten. ${nachricht}`.replace(
+      `Hallo, ich bin ${vorname} ${nachname} und ich möchte mit Ihnen zusammenarbeiten. ${nachricht}`.replace(
         / /g,
         "%20"
       );
 
-      form.current?.reset();
+    const url = `https://web.whatsapp.com/send?phone=+4915757790138&text=${message}`
+    // open URL in new tab
+
+    window.open(url, '_blank');
+
 
     return message;
   };
@@ -34,7 +45,7 @@ export default function Index() {
             Helfen sie dabei, ihr vorhaben abzusichern
           </p>
           <div className="mt-16 flex flex-col gap-16 sm:gap-y-20 lg:flex-row">
-            <form ref={form} method="POST" className="lg:flex-auto">
+            <div className="lg:flex-auto">
               <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                 <div>
                   <label
@@ -46,7 +57,7 @@ export default function Index() {
                   <div className="mt-2.5">
                     <input
                       type="text"
-                      onChange={(e) => setVorname(e.target.value)}
+                      ref={vorNameRef}
                       name="first-name"
                       id="first-name"
                       autoComplete="given-name"
@@ -64,7 +75,7 @@ export default function Index() {
                   <div className="mt-2.5">
                     <input
                       type="text"
-                      onChange={(e) => setNachname(e.target.value)}
+                      ref={nachNameRef}
                       name="last-name"
                       id="last-name"
                       autoComplete="family-name"
@@ -83,7 +94,7 @@ export default function Index() {
                     <textarea
                       id="message"
                       name="message"
-                      onChange={(e) => setNachricht(e.target.value)}
+                      ref={nachrichtRef}
                       rows={4}
                       className="block w-full rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                       defaultValue={""}
@@ -92,23 +103,21 @@ export default function Index() {
                 </div>
               </div>
               <div className="mt-10">
-                <Link
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={`https://web.whatsapp.com/send?phone=+4917661532329&text=${handleMessageForWhatsapp()}`}
+                <button
                   className="block w-full rounded-md bg-yellow-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick= {() => handleMessageForWhatsapp()}
                 >
                   Schreibe auf WhatsApp
-                </Link>
+                </button>
               </div>
-            </form>
+            </div>
             <div className="lg:mt-6 lg:w-80 lg:flex-none">
               <AiOutlineWhatsApp className="mx-auto h-16 w-16 text-lime-600" />
               <figure className="mt-10">
                 <blockquote className="text-lg font-semibold leading-8 text-gray-900">
                   <p>
-                    Kommen sie direkt mit mir in Kontakt, damit wir ihr
-                    unternehmen absichern können.
+                    Kommen Sie direkt mit mir in Kontakt, damit wir Ihr
+                    Unternehmen absichern können.
                   </p>
                   <p>
                     Bereits einige meiner Kunden haben mich auf diesem Weg
